f3ae18322aecc6969ec5dc951c3d5ade52dbab2b
wcs7777 <wcs777git@gmail.com>
2025-07-25 02:31:27 -0300

    feat: only auto-save if not in insert mode

--- lua/user/plugins/auto-save.lua
+++ lua/user/plugins/auto-save.lua
@@ -2,10 +2,26 @@ return {
     "https://git.sr.ht/~nedia/auto-save.nvim",
     event = { "BufReadPre" },
     opts = {
-        events = { 'InsertLeave' },
+        events = { 'InsertLeave', 'TextChanged' },
         silent = false,
-        save_cmd = 'update',
-        timeout = 5000,
+        save_fn = nil,
+        timeout = 500,
         exclude_ft = { 'neo-tree' },
     },
+    config = function(_, opts)
+        local autosave = require('auto-save')
+        opts.save_fn = function()
+            if vim.fn.mode() == 'i' then
+                return
+            end
+            if opts.save_cmd ~= nil then
+                vim.cmd(opts.save_cmd)
+            elseif opts.silent then
+                vim.cmd('silent! write')
+            else
+                vim.cmd('write')
+            end
+        end
+        autosave.setup(opts)
+    end,
 }
