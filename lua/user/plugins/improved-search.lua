6d9f5bbda9e5bdcb696f7b7016703a3ec943a411
wcs7777 <wcs777git@gmail.com>
2025-09-10 13:25:03 -0300

    reorder lines

--- lua/user/plugins/improved-search.lua
+++ lua/user/plugins/improved-search.lua
@@ -8,8 +8,8 @@ return {
 	config = function(_, opts)
 		local search = require("improved-search")
 		search.setup(opts)
-		vim.keymap.set("v", "!", search.in_place, desc("Search selected text"))
 		vim.keymap.set("v", "|", search.in_place_strict, desc("Search selected text strictly"))
+		vim.keymap.set("v", "!", search.in_place, desc("Search selected text"))
 		vim.keymap.set("n", "!", search.current_word, desc("Search current word without moving"))
 	end,
 }
