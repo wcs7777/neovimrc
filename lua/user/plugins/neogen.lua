ce06a1389a98d05db18df9a1a7b0014784971238
wcs7777 <wcs777git@gmail.com>
2025-12-01 11:26:53 -0300

    feat(neogen): add function annotation with <F3> keymap

--- lua/user/plugins/neogen.lua
+++ lua/user/plugins/neogen.lua
@@ -14,6 +14,7 @@ return {
 		neogen.setup(opts)
 		vim.keymap.set("n", "<leader>ncg", neogen.generate, desc("Neogen generate annotation"))
 		vim.keymap.set("n", "<leader>ncc", generate("func"), desc("Neogen generate function annotation"))
+		vim.keymap.set("n", "<F3>", generate("func"), desc("Neogen generate function annotation"))
 		vim.keymap.set("n", "<leader>nck", generate("class"), desc("Neogen generate class annotation"))
 		vim.keymap.set("n", "<leader>nct", generate("type"), desc("Neogen generate type annotation"))
 		vim.keymap.set("n", "<leader>ncf", generate("file"), desc("Neogen generate file annotation"))
