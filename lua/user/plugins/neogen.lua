c28618cd5f43f69c62c7df12eae64424a39c488f
wcs7777 <wcs777git@gmail.com>
2025-12-01 11:25:26 -0300

    feat: add neogen - annotation generator

---
+++ lua/user/plugins/neogen.lua
@@ -0,0 +1,21 @@
+local desc = require("user.utils").desc
+
+return {
+	"danymat/neogen",
+	opts = {},
+	config = function(_, opts)
+		local neogen = require('neogen')
+		---@param type string   func, class, type or file
+		local function generate(type)
+			return function()
+				neogen.generate({ type = type })
+			end
+		end
+		neogen.setup(opts)
+		vim.keymap.set("n", "<leader>ncg", neogen.generate, desc("Neogen generate annotation"))
+		vim.keymap.set("n", "<leader>ncc", generate("func"), desc("Neogen generate function annotation"))
+		vim.keymap.set("n", "<leader>nck", generate("class"), desc("Neogen generate class annotation"))
+		vim.keymap.set("n", "<leader>nct", generate("type"), desc("Neogen generate type annotation"))
+		vim.keymap.set("n", "<leader>ncf", generate("file"), desc("Neogen generate file annotation"))
+	end,
+}
