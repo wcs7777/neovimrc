a4192a0533164f1a8db8572d0379bcc55dfe6a25
wcs7777 <wcs777git@gmail.com>
2026-01-02 15:37:49 -0300

    feat(autosave): toggle enabled/disabled with keymap

--- lua/user/plugins/auto-save.lua
+++ lua/user/plugins/auto-save.lua
@@ -1,3 +1,5 @@
+local desc = require('user.utils').desc
+
 return {
     'wcs7777/auto-save.nvim',
     event = { 'BufReadPre' },
@@ -10,8 +12,20 @@ return {
     },
     config = function(_, opts)
         local autosave = require('auto-save')
+
+        local enabled = true
+        local function toggle()
+            enabled = not enabled
+            vim.notify('Auto save ' .. (enabled and 'enabled' or 'disabled'))
+        end
+
+        vim.keymap.set('n', '<leader>tsa', toggle, desc('Toggle autosave'))
+
         ---@param bufnr integer
         opts.save_fn = function(bufnr)
+            if not enabled then
+                return
+            end
             if (
                 bufnr ~= vim.api.nvim_get_current_buf() or
                 not vim.api.nvim_get_option_value('modified', { buf = bufnr }) or
@@ -26,6 +40,7 @@ return {
                 vim.cmd(save_cmd)
             end
         end
+
         autosave.setup(opts)
     end,
 }
