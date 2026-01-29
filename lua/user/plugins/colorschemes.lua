7678bb604225ee48f25c7fb5a612f3e7164c3589
wcs7777 <wcs777git@gmail.com>
2026-01-29 16:35:16 -0300

    feat(colorschemes): use plugin config when switching colorscheme

--- lua/user/plugins/colorschemes.lua
+++ lua/user/plugins/colorschemes.lua
@@ -1,13 +1,13 @@
-vim.opt.background = 'light'
+local transparent = false
+local terminal_theme = 'light'
+local current_colorscheme = 'newpaper'
 
-return {
+local colorschemes = {
     {
         'yorik1984/newpaper.nvim',
         name = 'newpaper',
-        lazy = false,
-        priority = 1000,
         opts = {
-            style = 'light',
+            style = terminal_theme,
             preset = {},
             hide_eob = true,
             colors = {
@@ -27,45 +27,127 @@ return {
             italic_functions    = false,
             italic_variables    = false,
         },
-        config = function(_, opts)
+        setup = function(opts)
             require('newpaper').setup(opts)
-            vim.cmd('colorscheme newpaper')
+            vim.opt.background = opts.style
         end,
     },
     {
         'navarasu/onedark.nvim',
-        priority = 1000,
-        lazy = true,
-        config = function()
-            require('onedark').setup {
-                style = 'light'
-            }
-            -- Enable theme
-            require('onedark').load()
-        end
+        name = 'onedark',
+        opts = {
+        },
+        enable = function() require('onedark').load() end,
     },
     {
         'rose-pine/neovim',
         name = 'rose-pine',
-        lazy = true,
-        priority = 1000,
-        config = function()
-            require('rose-pine').setup({
-                disable_italics = true,
-            })
-            vim.cmd('colorscheme rose-pine')
-        end
+        opts = {
+            styles = {
+                italic = false,
+            },
+        },
     },
     {
         'catppuccin/nvim',
         name = 'catppuccin',
-        lazy = true,
-        priority = 1000,
-        config = function()
-            require('catppuccin').setup({
-                no_italics = true,
-            })
-            vim.cmd('colorscheme catppuccin')
-        end
+        opts = {
+            no_italics = true,
+        },
     },
 }
+
+local lazy_colorschemes = {}
+local name2setup = {}
+
+for _, cs in ipairs(colorschemes) do
+    local setup = (function(name, setup, opts)
+        return function()
+            if setup then
+                setup(opts)
+            else
+                require(name).setup(opts)
+            end
+        end
+    end)(cs.name, cs.setup, cs.opts or {})
+    local enable = (function(enable)
+        return function()
+            if enable then
+                enable()
+            else
+                vim.cmd('colorscheme ' .. cs.name)
+            end
+        end
+    end)(cs.enable)
+    local is_current = current_colorscheme:sub(1, #cs.name) == cs.name
+    table.insert(lazy_colorschemes, {
+        cs[1],
+        name = cs.name,
+        lazy = not is_current,
+        priority = is_current and 1000 or 50,
+        opts = cs.opts or {},
+        config = function()
+            setup()
+            enable()
+        end,
+    })
+    name2setup[cs.name] = setup
+end
+
+vim.api.nvim_create_autocmd('ColorScheme', {
+    group = vim.api.nvim_create_augroup('user-colorscheme', { clear = true }),
+    pattern = '*',
+    callback = function(args)
+        local setup = name2setup[args.match]
+        if setup == nil then
+            for key, value in pairs(name2setup) do
+                if args.match:sub(1, #key) == key then
+                    setup = value
+                    break
+                end
+            end
+        end
+        if setup ~= nil then
+            setup()
+        end
+        if transparent and vim.opt.background:get() == terminal_theme then
+            local groups = {
+                'Comment',
+                'Conditional',
+                'Constant',
+                'CursorLine',
+                'CursorLineNr',
+                'EndOfBuffer',
+                'Function',
+                'Identifier',
+                'LineNr',
+                'NonText',
+                'Normal',
+                'NormalNC',
+                'Operator',
+                'PreProc',
+                'Repeat',
+                'SignColumn',
+                'Special',
+                'Statement',
+                'StatusLine',
+                'StatusLineNC',
+                'String',
+                'Structure',
+                'Todo',
+                'Type',
+                'Underlined',
+            }
+            for _, name in ipairs(groups) do
+                local hl = vim.api.nvim_get_hl(0, { name = name })
+                if next(hl) ~= nil then
+                    local new_hl = vim.tbl_extend('force', hl, { bg = 'none', ctermbg = 'none' })
+                    new_hl[true] = nil
+                    vim.api.nvim_set_hl(0, name, new_hl)
+                end
+            end
+        end
+    end,
+})
+
+return lazy_colorschemes
