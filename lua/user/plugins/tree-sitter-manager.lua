baba7c4cf0d33d9a64453ef910a79e454cea5ca4
wcs7777 <wcs777git@gmail.com>
2026-06-01 02:10:20 -0300

    fix(tree-sitter-manager): remove config block to use opt

--- lua/user/plugins/tree-sitter-manager.lua
+++ lua/user/plugins/tree-sitter-manager.lua
@@ -13,14 +13,4 @@ return {
 		},
 		auto_install = true,
 	},
-	config = function()
-		require("tree-sitter-manager").setup({
-			-- Default Options
-			-- ensure_installed = {}, -- list of parsers to install at the start of a neovim session. If set to "all", install all parsers.
-			-- border = nil, -- border style for the window (e.g. "rounded", "single"), if nil, use the default border style defined by 'vim.o.winborder'. See :h 'winborder' for more info.
-			-- auto_install = false, -- if enabled, install missing parsers when editing a new file
-			-- highlight = true, -- treesitter highlighting is enabled by default
-			-- languages = {}, -- override or add new parser sources
-		})
-	end
 }
