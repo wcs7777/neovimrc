23cf34f59196c4d33481eca289e20868195c604e
wcs7777 <wcs777git@gmail.com>
2026-09-04 04:01:26 -0300

    feat: prevent opening large files

--- lua/user/core/autocmds.lua
+++ lua/user/core/autocmds.lua
@@ -25,3 +25,28 @@ vim.api.nvim_create_autocmd("FileType", {
 		vim.opt_local.expandtab = true
 	end,
 })
+
+vim.api.nvim_create_autocmd("BufReadPre", {
+	callback = function(args)
+		local max_file_size = 10 * 1024 * 1024 -- 10 MB
+		local stat = vim.uv.fs_stat(args.file)
+		if not stat or stat.type ~= "file" or stat.size <= max_file_size then
+			return
+		end
+		local size_mb = stat.size / 1024 / 1024
+		local name = args.file
+		local close = false
+		vim.ui.select({ "Cancel", "Open" }, {
+			prompt = string.format("%s is %.1f MiB. Open anyway?", name, size_mb),
+		}, function(choice)
+			close = choice ~= "Open"
+			vim.notify(choice)
+		end)
+		if close then
+			vim.notify("Closing file", vim.log.levels.ERROR)
+			vim.schedule(function()
+				vim.api.nvim_buf_delete(args.buf, { force = true })
+			end)
+		end
+	end,
+})
